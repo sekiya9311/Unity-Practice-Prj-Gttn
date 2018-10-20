4a800773587b562dcdb21086546aea0831b30388
sekiya9311 <sekiya9311@gmail.com>
2018-10-20 17:09:09 +0900

    いろいろ

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float speedPerSecond = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveDirection = new Vector3(1, 0, 0);
+        this.transform.position += moveDirection.normalized * speedPerSecond * Time.deltaTime;
+	}
+}
