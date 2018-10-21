ed902b98634270cbddf56fd69f0f90d32c275693
sekiya9311 <sekiya9311@gmail.com>
2018-10-22 00:24:08 +0900

    アホな動き追加

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float speedPerSecond = 2.0f;
+    private int cntEven = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,25 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //this.Buruburu();
+        this.GoStraight();
+	}
+
+    /// <summary>
+    /// どっか行きます
+    /// </summary>
+    private void GoStraight()
+    {
         Vector3 moveDirection = new Vector3(1, 0, 0);
         this.transform.position += moveDirection.normalized * speedPerSecond * Time.deltaTime;
-	}
+    }
+
+    /// <summary>
+    /// ブルブルします
+    /// </summary>
+    private void Buruburu()
+    {
+        Vector3 moveDirection = new Vector3(1, 0, 0);
+        this.transform.position += moveDirection.normalized * speedPerSecond * Time.deltaTime * (cntEven *= -1);
+    }
 }
