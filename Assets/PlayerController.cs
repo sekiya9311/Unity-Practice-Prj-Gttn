e199bb45d94ea3df494e2604f65dcbeba60364fb
sekiya9311 <sekiya9311@gmail.com>
2018-10-22 00:30:21 +0900

    アホなの追加

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float speedPerSecond = 2.0f;
     private int cntEven = 5;
+    private Vector3[] vec3Dirs = { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) };
+    private int dirsCnt = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
     
@@ -16,7 +18,8 @@ public class PlayerController : MonoBehaviour {
 	void Update ()
     {
         //this.Buruburu();
-        this.GoStraight();
+        //this.GoStraight();
+        this.Fly();
 	}
 
     /// <summary>
@@ -35,5 +38,14 @@ public class PlayerController : MonoBehaviour {
     {
         Vector3 moveDirection = new Vector3(1, 0, 0);
         this.transform.position += moveDirection.normalized * speedPerSecond * Time.deltaTime * (cntEven *= -1);
+    }
+
+    /// <summary>
+    /// どっか飛んでく
+    /// </summary>
+    private void Fly()
+    {
+        this.transform.position += vec3Dirs[dirsCnt].normalized * speedPerSecond * Time.deltaTime;
+        dirsCnt = (dirsCnt + 2) % 3;
     }
 }
