091eb9840dab554e46b7287516fea1eba832b77a
sekiya9311 <sekiya9311@gmail.com>
2018-10-29 22:02:45 +0900

    add Bullet object

---
+++ Assets/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    [SerializeField] private float speedPerSecond = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speedPerSecond * Time.deltaTime;
+    }
+}
