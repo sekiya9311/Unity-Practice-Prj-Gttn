14d208115a736da2d6497c1c71705173fc488dce
sekiya9311 <sekiya9311@gmail.com>
2018-11-01 00:59:21 +0900

    雑にログでクリア判定

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform followTarget = null;
+    private Vector3 cameraPosition = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        if (this.followTarget)
+        {
+            this.cameraPosition = this.transform.position - this.followTarget.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.followTarget)
+        {
+            this.transform.position = this.followTarget.position + this.cameraPosition;
+        }
+    }
+}
--- Assets/FieldController.cs
+++ Assets/FieldController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq;
+
 public class FieldController : MonoBehaviour
 {
 
     [SerializeField] private GameObject enemyPrefab = null;
+    private IEnumerable<GameObject> enemies = null;
 
     // Use this for initialization
     void Start()
@@ -15,22 +18,23 @@ public class FieldController : MonoBehaviour
             Debug.LogWarning($"Please set Bullet as {nameof(this.enemyPrefab)} !!");
             return;
         }
-        var enemCnt = Random.Range(0, 10);
-        for (int i = 0; i < enemCnt; i++)
-        {
-            Instantiate(
-                this.enemyPrefab, 
+
+        this.enemies = Enumerable.Range(0, Random.Range(1, 100))
+            .Select(i => Instantiate(
+                this.enemyPrefab,
                 new Vector3(
-                    Random.Range(0, 10), 
-                    0.5f, 
-                    Random.Range(0, 10)),
-                new Quaternion(0,0,0,0));
-        }
+                    Random.Range(-20, 20),
+                    0.5f,
+                    Random.Range(-20, 20)),
+                new Quaternion(0, 0, 0, 0))).ToList();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (this.enemies.All(e => e == null))
+        {
+            Debug.Log("CLEAR !!");
+        }
     }
 }
